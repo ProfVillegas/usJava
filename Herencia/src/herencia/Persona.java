774fded11b6496a0622c85bfc947f36cb0237e1d
ProfVillegas <julian.villegas@utrivieramaya.edu.mx>
2019-06-01 10:42:37 -0500

    Herencia

---
+++ Herencia/src/herencia/Persona.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author julia
+ */
+public class Persona {
+    public String nombre;
+    public String apellido;
+    
+    Persona(){
+        this.nombre="Sin nombre";
+        this.apellido="Sin apellido";
+    }
+}
---
+++ Herencia/src/herencia/Recepcionista.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author julia
+ */
+public class Recepcionista extends Persona{
+    public int id_recepcion;
+    
+    Recepcionista(){
+        super();
+        this.id_recepcion=0;
+    }
+    
+}
